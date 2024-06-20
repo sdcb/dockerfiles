5459e6ca491daae6ade063fa88c967c2ebcf4d62
sdcb <sdflysha@qq.com>
2024-06-20 22:11:40 +0800

    remove aliyun in choco

--- scripts/docker-utils.ps1
+++ scripts/docker-utils.ps1
@@ -1,25 +1,22 @@
 function Login-DockerRegistries {
     param(
-        [string]$aliyunUsername,
-        [string]$aliyunPassword,
-        [string]$aliyunRegistry,
         [string]$dockerUsername,
-        [string]$dockerPassword
+        [string]$dockerToken,
+        [string]$githubOwner,
+        [string]$githubToken
     )
 
-    # Login to Alibaba Cloud Container Registry
-    Write-Output $aliyunPassword | docker login -u $aliyunUsername $aliyunRegistry --password-stdin
+    # Login to GitHub Container Registry
+    Write-Output $githubToken | docker login ghcr.io -u $githubOwner --password-stdin
 
     # Login to Docker Hub
-    Write-Output $dockerPassword | docker login -u $dockerUsername --password-stdin
+    Write-Output $dockerToken | docker login -u $dockerUsername --password-stdin
 }
 
 function TagAndPushDockerImages {
     param(
         [string]$imageName,
         [string]$githubRepository,
-        [string]$aliyunRegistry,
-        [string]$aliyunNamespace,
         [string]$dockerUsername,
         [int]$runId
     )
@@ -31,68 +28,27 @@ function TagAndPushDockerImages {
     docker tag $imageName "ghcr.io/${githubRepository}/${imageName}:$date"
     docker tag $imageName "ghcr.io/${githubRepository}/${imageName}"
 
-    # Tagging for Alibaba Cloud
-    docker tag $imageName "${aliyunRegistry}/${aliyunNamespace}/${imageName}:r-$runId"
-    docker tag $imageName "${aliyunRegistry}/${aliyunNamespace}/${imageName}:$date"
-    docker tag $imageName "${aliyunRegistry}/${aliyunNamespace}/${imageName}"
-
     # Tagging for Docker Hub
     docker tag $imageName "${dockerUsername}/${imageName}:r-$runId"
     docker tag $imageName "${dockerUsername}/${imageName}:$date"
     docker tag $imageName "${dockerUsername}/${imageName}"
 
-    # Script block for pushing to ghcr.io
-    $githubPush = {
-        $sw = [System.Diagnostics.Stopwatch]::StartNew()
-        docker push "ghcr.io/${using:githubRepository}/${using:imageName}:r-${using:runId}"
-        docker push "ghcr.io/${using:githubRepository}/${using:imageName}:${using:date}"
-        docker push "ghcr.io/${using:githubRepository}/${using:imageName}"
-        $sw.Stop()
-        Write-Output "Time taken to push to GitHub Container Registry: $($sw.Elapsed.TotalSeconds) seconds"
-    }
+    # pushing to ghcr.io
+    Write-Host "Pushing to GitHub Container Registry..."
+    $sw = [System.Diagnostics.Stopwatch]::StartNew()
+    docker push "ghcr.io/${using:githubRepository}/${using:imageName}:r-${using:runId}"
+    docker push "ghcr.io/${using:githubRepository}/${using:imageName}:${using:date}"
+    docker push "ghcr.io/${using:githubRepository}/${using:imageName}"
+    $sw.Stop()
+    Write-Output "Time taken to push to GitHub Container Registry: $($sw.Elapsed.TotalSeconds) seconds"
 
-    # Script block for pushing to Alibaba Cloud
-    $aliyunPush = {
-        $sw = [System.Diagnostics.Stopwatch]::StartNew()
-        docker push "${using:aliyunRegistry}/${using:aliyunNamespace}/${using:imageName}:r-${using:runId}"
-        docker push "${using:aliyunRegistry}/${using:aliyunNamespace}/${using:imageName}:${using:date}"
-        docker push "${using:aliyunRegistry}/${using:aliyunNamespace}/${using:imageName}"
-        $sw.Stop()
-        Write-Output "Time taken to push to Alibaba Cloud: $($sw.Elapsed.TotalSeconds) seconds"
-    }
-
-    # Script block for pushing to Docker Hub
-    $dockerHubPush = {
-        $sw = [System.Diagnostics.Stopwatch]::StartNew()
-        docker push "${using:dockerUsername}/${using:imageName}:r-${using:runId}"
-        docker push "${using:dockerUsername}/${using:imageName}:${using:date}"
-        docker push "${using:dockerUsername}/${using:imageName}"
-        $sw.Stop()
-        Write-Output "Time taken to push to Docker Hub: $($sw.Elapsed.TotalSeconds) seconds"
-    }
-
-    $selectedPushes = @($githubPush, $dockerHubPush) # removed $aliyunPush because too slow
-    Write-Output "Pushing to Registries..."
-
-    # Parallel execution of push operations
-    $jobs = @()
-    foreach ($push in $selectedPushes) {
-        $jobs += Start-Job -ScriptBlock $push
-    }
-
-    # Wait for all jobs to complete and stream their output
-    $jobs | ForEach-Object {
-        # Continuously check job status and fetch output
-        while ($_.State -eq 'Running') {
-            # Receive available output without removing it from the job
-            $_ | Receive-Job -Keep
-            Start-Sleep -Seconds 1
-        }
-    
-        # Receive any remaining output once the job is completed
-        $_ | Receive-Job
-        Remove-Job $_
-    }
+    # pushing to Docker Hub
+    $sw = [System.Diagnostics.Stopwatch]::StartNew()
+    docker push "${using:dockerUsername}/${using:imageName}:r-${using:runId}"
+    docker push "${using:dockerUsername}/${using:imageName}:${using:date}"
+    docker push "${using:dockerUsername}/${using:imageName}"
+    $sw.Stop()
+    Write-Output "Time taken to push to Docker Hub: $($sw.Elapsed.TotalSeconds) seconds"
 
     Write-Output "All pushes completed."
 }
