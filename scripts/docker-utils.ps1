f8e2062410740e0a75f425f59258e92a9584d70a
sdcb <sdflysha@qq.com>
2024-06-20 18:48:31 +0800

    test

--- scripts/docker-utils.ps1
+++ scripts/docker-utils.ps1
@@ -8,10 +8,10 @@ function Login-DockerRegistries {
     )
 
     # Login to Alibaba Cloud Container Registry
-    echo $aliyunPassword | docker login -u $aliyunUsername $aliyunRegistry --password-stdin
+    Write-Output $aliyunPassword | docker login -u $aliyunUsername $aliyunRegistry --password-stdin
 
     # Login to Docker Hub
-    echo $dockerPassword | docker login -u $dockerUsername --password-stdin
+    Write-Output $dockerPassword | docker login -u $dockerUsername --password-stdin
 }
 
 function TagAndPushDockerImages {
@@ -28,22 +28,22 @@ function TagAndPushDockerImages {
     $date = Get-Date -Format "yyyyMMdd"
 
     # Tagging for Alibaba Cloud
-    docker tag $imageName "$aliyunRegistry/$aliyunNamespace/$imageName"
-    docker tag $imageName "$aliyunRegistry/$aliyunNamespace/$imageName:$date"
-    docker tag $imageName "$aliyunRegistry/$aliyunNamespace/$imageName:r-$runId"
+    docker tag $imageName "${aliyunRegistry}/${aliyunNamespace}/${imageName}"
+    docker tag $imageName "${aliyunRegistry}/${aliyunNamespace}/${imageName}:$date"
+    docker tag $imageName "${aliyunRegistry}/${aliyunNamespace}/${imageName}:r-$runId"
 
     # Tagging for Docker Hub
-    docker tag $imageName "$dockerUsername/$imageName"
-    docker tag $imageName "$dockerUsername/$imageName:$date"
-    docker tag $imageName "$dockerUsername/$imageName:r-$runId"
+    docker tag $imageName "${dockerUsername}/${imageName}"
+    docker tag $imageName "${dockerUsername}/${imageName}:$date"
+    docker tag $imageName "${dockerUsername}/${imageName}:r-$runId"
 
     # Pushing to Alibaba Cloud
-    docker push "$aliyunRegistry/$aliyunNamespace/$imageName"
-    docker push "$aliyunRegistry/$aliyunNamespace/$imageName:$date"
-    docker push "$aliyunRegistry/$aliyunNamespace/$imageName:r-$runId"
+    docker push "${aliyunRegistry}/${aliyunNamespace}/${imageName}"
+    docker push "${aliyunRegistry}/${aliyunNamespace}/${imageName}:$date"
+    docker push "${aliyunRegistry}/${aliyunNamespace}/${imageName}:r-$runId"
 
     # Pushing to Docker Hub
-    docker push "$dockerUsername/$imageName"
-    docker push "$dockerUsername/$imageName:$date"
-    docker push "$dockerUsername/$imageName:r-$runId"
+    docker push "${dockerUsername}/${imageName}"
+    docker push "${dockerUsername}/${imageName}:$date"
+    docker push "${dockerUsername}/${imageName}:r-$runId"
 }
