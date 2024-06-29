02d339aa38ce3fa105464a944442fc713fdc0441
sdcb <sdflysha@qq.com>
2024-06-29 09:02:10 +0800

    cp first.

--- .github/workflows/paddle-windows-build/openblas-noavx.ps1
+++ .github/workflows/paddle-windows-build/openblas-noavx.ps1
@@ -6,7 +6,7 @@ cd C:\Paddle\Build
 cmake .. -GNinja -DWITH_AVX=OFF -DWITH_MKL=OFF -DWITH_GPU=OFF -DON_INFER=ON -DWITH_PYTHON=OFF -DWITH_ONNXRUNTIME=ON -DWITH_UNITY_BUILD=ON
 ninja all
 
+cp C:\Paddle\Build\paddle_inference_install_dir\paddle\lib\common* C:\Paddle\Build\paddle_inference_c_install_dir\paddle\lib
 Compress-Archive -Path C:\Paddle\Build\paddle_inference_c_install_dir\* -DestinationPath C:\Paddle\Build\c.zip
 Compress-Archive -Path C:\Paddle\Build\paddle_inference_install_dir\* -DestinationPath C:\Paddle\Build\cpp.zip
-cp C:\Paddle\Build\paddle_inference_install_dir\paddle\lib\common* C:\Paddle\Build\paddle_inference_c_install_dir\paddle\lib
 ls C:\Paddle\Build
